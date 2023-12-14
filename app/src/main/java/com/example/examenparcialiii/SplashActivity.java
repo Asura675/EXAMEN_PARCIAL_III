05a4d37bcce2cd39217950379b3915474cf7da17
Asura675 <miguel_m19@outlook.com>
2023-12-14 11:06:41 -0500

    Splash screen funcional

--- app/src/main/java/com/example/examenparcialiii/SplashActivity.java
+++ app/src/main/java/com/example/examenparcialiii/SplashActivity.java
@@ -9,10 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.examenparcialiii.R;
 
 public class SplashActivity extends AppCompatActivity {
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
+        setContentView(R.layout.splashscreenactivity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 4000);  // 4 segundos de pausa antes de iniciar la MainActivity
     }
 }
